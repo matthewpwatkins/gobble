5b7cab470d005b0ff12e6e28a03544f4c7d3ff56
Matthew Watkins <3808901+matthewpwatkins@users.noreply.github.com>
2024-05-15 17:16:57 -0500

    Good pause point

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const grid = Array.from({ length: 5 }, (_, i) => Array(5).fill(i));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">
+        {grid.map((row, i) =>
+          row.map((_, j) => <div key={`${i}-${j}`} className="grid-item">A</div>)
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
